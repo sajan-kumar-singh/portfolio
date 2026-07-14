9b403f4cd7f5aafc795f4fb907ca69ee2b735960
sajan-kumar-singh <saajan.199923@gmail.com>
2026-07-14 15:37:28 +0530

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,13 @@ import './index.css';
 function App() {
   return (
     <div>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/">Portfolio</a></li>
+          <li><a href="/">Contact</a></li>
+        </ul>
+      </nav>
       <h1>Sajan</h1>
     </div>
   );
