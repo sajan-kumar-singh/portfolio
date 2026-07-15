4b0c698b947c3892fb01f6d55846271263d286c4
sajan-kumar-singh <saajan.199923@gmail.com>
2026-07-15 16:36:15 +0530

    added circle

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,23 @@ function App() {
           <li><a href="/">Contact</a></li>
         </ul>
       </nav>
-      <h1>Sajan</h1>
+      <div className="banner">
+        <div className="ring ring-outer"></div>
+        <div className="ring ring-inner"></div>
+
+        {/* Outer Orbit - 3 Images */}
+        <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="outer-orbit-1" className="orbit-icon icon-outer" style={{ filter: 'invert(1)', animationDelay: '0s' }} />
+        <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="outer-orbit-2" className="orbit-icon icon-outer" style={{ filter: 'invert(1)', animationDelay: '-1s' }} />
+        <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="outer-orbit-3" className="orbit-icon icon-outer" style={{ filter: 'invert(1)', animationDelay: '-2s' }} />
+        <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="outer-orbit-4" className="orbit-icon icon-outer" style={{ filter: 'invert(1)', animationDelay: '-3s' }} />
+        <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="outer-orbit-4" className="orbit-icon icon-outer" style={{ filter: 'invert(1)', animationDelay: '-4s' }} />
+
+        {/* Inner Orbit - 3 Images */}
+        <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="inner-orbit-1" className="orbit-icon icon-inner" style={{ filter: 'invert(1)', animationDelay: '0s' }} />
+        <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="inner-orbit-2" className="orbit-icon icon-inner" style={{ filter: 'invert(1)', animationDelay: '-1s' }} />
+        <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="inner-orbit-3" className="orbit-icon icon-inner" style={{ filter: 'invert(1)', animationDelay: '-2s' }} />
+        <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="inner-orbit-4" className="orbit-icon icon-inner" style={{ filter: 'invert(1)', animationDelay: '-3s' }} />
+      </div>
     </div>
   );
 }
